60172eae09a55ec46777d00979d16301d352319b
unknown <cawfee4@gmail.com>
2024-07-10 02:22:32 +0700

    change icon

--- antd-demo/src/app/components/CommentModal.jsx
+++ antd-demo/src/app/components/CommentModal.jsx
@@ -3,23 +3,24 @@
 "use client";
 
 import React, { useState } from "react";
-import { Modal, Button, Form, Input, message } from "antd";
+import { Modal, Button, Form, Input, message, Spin } from "antd";
 import base_route from "../utils/routing";
 
 const { TextArea } = Input;
 
 const CommentModal = ({ imagePostId, visible, onClose }) => {
   const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
 
   const handleFinish = async (values) => {
+    setLoading(true);
     try {
       const response = await fetch(`${base_route}/comment`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json", // Specify content type JSON
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          // Convert body to JSON string
           content: values.content,
           imagePostId: imagePostId,
         }),
@@ -30,13 +31,14 @@ const CommentModal = ({ imagePostId, visible, onClose }) => {
       }
 
       // Handle success
-      // Reset form fields and close modal or whatever your logic is
       form.resetFields();
       message.success("Comment added successfully.");
       onClose();
     } catch (error) {
       console.error("Upload error:", error);
       message.error("Upload failed.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -47,20 +49,22 @@ const CommentModal = ({ imagePostId, visible, onClose }) => {
       onCancel={onClose}
       footer={null}
     >
-      <Form form={form} onFinish={handleFinish} layout="vertical">
-        <Form.Item
-          name="content"
-          label="Comment"
-          rules={[{ required: true, message: "Please enter your comment" }]}
-        >
-          <TextArea rows={4} />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <Spin spinning={loading}>
+        <Form form={form} onFinish={handleFinish} layout="vertical">
+          <Form.Item
+            name="content"
+            label="Comment"
+            rules={[{ required: true, message: "Please enter your comment" }]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" disabled={loading}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Spin>
     </Modal>
   );
 };
--- antd-demo/src/app/layout.js
+++ antd-demo/src/app/layout.js
@@ -1,8 +1,25 @@
 import React from "react";
+import Head from "next/head";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { icons } from "antd/es/image/PreviewGroup";
+
+export const metadata = {
+  title: "Gallery",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+  },
+  apple: {
+    icon: ["/apple-touch-icon.png"],
+  },
+  shortcut: ["/apple-touch-icon.png"],
+  manifest: "/site.webmanifest",
+};
 
 const RootLayout = ({ children }) => (
   <html lang="en">
+    <Head>
+      <title>Default Title</title>
+    </Head>
     <body>
       <AntdRegistry>{children}</AntdRegistry>
     </body>
