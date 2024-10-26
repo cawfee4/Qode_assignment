ad23715b2347c87f1eb2bb9b47400dbb38647ebe
Bao Ninh <94046123+cawfee4@users.noreply.github.com>
2024-10-26 18:01:46 +0700

    add code (#9)

--- backend-demo/src/api/index.js
+++ backend-demo/src/api/index.js
@@ -16,6 +16,7 @@ cloudinary.config({
 console.log('feature 2')
 console.log('feature 1');
 
+console.log('feature 4');
 console.log('feature 3');
 
 router.post("/upload", upload.single("image"), async (req, res) => {
